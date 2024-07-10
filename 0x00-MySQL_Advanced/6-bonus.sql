f9352cbc05062809015bf3e2b8a42402e1e43b07
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-07-10 15:42:27 +0000

    add bonus

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,31 @@
+-- create stored procedure add bonus
+DELIMITER //
+
+CREATE PROCEDURE AddBonus (
+	IN user_id INT,
+	IN project_name VARCHAR(255),
+	IN score INT
+)
+BEGIN
+	-- declare variable to hold project ID
+	DECLARE project_id INT;
+
+	-- check if the project exists
+	SELECT id
+		INTO project_id
+		FROM projects
+		WHERE name = project_name;
+
+	-- if the project does not exist
+	IF project_id IS NULL THEN
+		INSERT INTO projects (name)
+			VALUES (project_name);
+		SET project_id = LAST_INSERT_ID();
+	END IF;
+
+	-- insert the correction
+	INSERT INTO corrections (user_id, project_id, score)
+		VALUES (user_id, project_id, score);
+END //
+
+DELIMITER ;
