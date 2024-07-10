4463000439b2e0f4c0bf149ab1fcacc442ce2dbe
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-07-10 12:41:41 +0000

    creating table users with enumeration

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- creates tables users with attributes
+
+-- create table users if does not exist
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
