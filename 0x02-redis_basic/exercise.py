0e6aed4ef47d3d2a882021e1a2eee5cc65c5c841
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-07-17 13:16:19 +0300

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -30,3 +30,51 @@ class Cache:
         random_uuid = str(uuid.uuid4())  # convert to string
         self._redis.set(random_uuid, data)
         return random_uuid  # returns the UUID key as a string
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable[[bytes], Union[str,
+                                                 bytes, int, float]]] = None
+            ) -> Union[str, bytes, int, float]:
+        """
+        Retrieve the data from the cache using the provided key.
+
+        Args:
+            key (str): The key to retrieve the data.
+            fn (Optional[Callable[[bytes], Union[str, bytes, int, float]]]):
+            A function to apply to the retrieved data.
+
+        Returns:
+            Union[str, bytes, int, float, None]: The retrieved data,
+            possibly transformed by fn, or None if the key does not exist.
+        """
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieve the data as a string from the cache using the provided key.
+
+        Args:
+            key (str): The key to retrieve the data.
+
+        Returns:
+            Optional[str]: The retrieved data as a string,
+            or None if the key does not exist.
+        """
+        return self.get(key, fn=lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieve the data as an integer from the cache using the provided key.
+
+        Args:
+            key (str): The key to retrieve the data.
+
+        Returns:
+            Optional[int]: The retrieved data as an integer,
+            or None if the key does not exist.
+        """
+        return self.get(key, fn=int)
